19aaef6f4bd49c0188c5ba09c7046d6c28f761fe
0D8K <98357173+0D8K@users.noreply.github.com>
2025-06-30 12:16:39 +0200

    Create appeareanceX.nix

---
+++ shared/appeareanceX.nix
@@ -0,0 +1,53 @@
+{ config, pkgs, ... }:
+
+{
+  ############################
+  ## PAQUETES DEL SISTEMA   ##
+  ############################
+
+  environment.systemPackages = with pkgs; [
+    i3
+    i3status
+    i3lock
+    dunst
+    kitty
+    rofi
+    xdg-utils
+    feh                    # fondo de pantalla
+    lxappearance           # cambiar temas GTK fácilmente
+    xclip                  # util para portapapeles
+    network-manager-applet
+    pulseaudio
+    pavucontrol
+    xterm
+    nerd-fonts
+    siji
+  ];
+
+  ############################
+  ## FUENTES                ##
+  ############################
+
+  fonts.packages = with pkgs; [
+    dejavu_fonts
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+  ];
+
+  ############################
+  ## HABILITAR X11 + I3     ##
+  ############################
+
+  services.xserver.enable = true;
+  services.xserver.windowManager.i3.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.xterm.enable = true;
+
+  # Entradas (ratón/teclado táctil, etc.)
+  services.xserver.libinput.enable = true;
+
+  # AutoLogin opcional
+  services.xserver.displayManager.autoLogin = {
+    enable = false;
+    user = "odbk";
+  };
+}
